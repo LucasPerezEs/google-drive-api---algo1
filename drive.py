0d45831cb8ea5ab4fe2d38bd80a3661f552e8954
“LucasPerezEs” <lucasperezesnaola@gmail.com>
2021-07-15 18:53:48 -0300

    reemplaze crear_archivo por subir_archivo, y funciona para cualquier tipo de archivo.  Defini crear_archivo y descargar_archivo

--- drive.py
+++ drive.py
@@ -1,37 +1,54 @@
 from service_drive import obtener_servicio
-from googleapiclient.http import MediaFileUpload
-import os
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+import os, io
 
 SERVICIO = obtener_servicio()
+MIME_TYPES = {"png": "image/png", "jpg": "image/jpeg", "jpeg": "image/jpeg", "pdf": "application/pdf", "txt": "text/plain", "bin": "application/octet-stream", "doc": "application/msword", "json": "application/json", "mp3": "audio/mpeg", "ppt": "application/vnd.ms-powerpoint", "rar": "application/vnd.rar", "xls": "application/vnd.ms-excel", "zip": "application/zip"}
 
-def crear_archivo():
-    name = input("Ingrese el nombre (con la extension) del archivo que desea crear: ")
-    metadata = {"name" : name}
-
-    directorio = os.path.dirname(os.path.realpath(name))
-    ruta = os.path.join(directorio, name)
+def definir_mime_type(nombre_archivo: str) -> str:
+    nombre_separado = nombre_archivo.split(".")
+    extension = nombre_separado[1]
+    mime = MIME_TYPES[extension]
     
+    return mime
+
+def subir_archivo(nombre_archivo: str):
+    metadata = {"name" : nombre_archivo}
+    directorio = os.path.dirname(os.path.realpath(nombre_archivo))
+    ruta = os.path.join(directorio, nombre_archivo)
+        
     parents = []
-    carpeta = input("Si desea que el archivo este adentro de una carpeta en Drive, copie y pegue el ID de la misma aqui. De lo contrario, presione ENTER. ")
-    while carpeta != "":
-        parents.append(carpeta)
-        carpeta = input("Si desea crear el archivo en otra subcarpeta, copie y pegue el ID de la misma aqui. ")
-    
+    opcion = input("Desea crear el archivo en una carpeta? (s/n) ").lower()
+    if opcion == "s":
+        opcion = int(input("Desea crear una carpeta o usar una existente?\n 1_Nueva Carpeta | 2_ Carpeta Existente "))
+        
+        if opcion == 1:
+            crear_carpeta()
+            carpeta = input("Copie y pegue el ID de la carpeta recien creada aqui. ")
+            parents.append(carpeta)
+        else:
+            carpeta = input("Copie y pegue el ID de la carpeta donde desea crear el archivo ")
+            parents.append(carpeta)
+            
     if len(parents) != 0:
-        for i in range(len(parents)):
-            metadata["parents"] = parents[i]        
+        metadata["parents"] = parents        
 
-    media = MediaFileUpload(ruta, mimetype="image/jpeg")
+    mime = definir_mime_type(nombre_archivo)
+    media = MediaFileUpload(ruta, mimetype=mime)
     subir = SERVICIO.files().create(body=metadata, media_body=media, fields="id").execute()
 
     if subir:
-        print(f"{name} fue subido con exito. \n")
+        print(f"{nombre_archivo} fue subido con exito. \n")
 
-def crear_carpeta():
+def crear_archivo():
+    crear = SERVICIO.files().create().execute()
+    if crear:
+        print("El archivo se ha creado con exito. ")
+
+def crear_carpeta(nombre_carpeta: str):
     metadata = {}
     
-    nombre = input("Ingrese el nombre de la carpeta que desea crear:  ")
-    metadata["name"] = nombre
+    metadata["name"] = nombre_carpeta
     
     mime_type = "application/vnd.google-apps.folder"
     metadata["mimeType"] = mime_type
@@ -53,6 +70,24 @@ def crear_carpeta():
     subir = SERVICIO.files().create(body=metadata).execute()
 
     if subir:
-        print(f"\nLa carpeta {nombre} fue creada con exito. ")
+        print(f"\nLa carpeta {nombre_carpeta} fue creada con exito. ")
 
-crear_carpeta()
+def descargar_archivo(id_archivo: str, nombre_archivo: str):
+    
+    descargar = SERVICIO.files().get_media(fileId=id_archivo)
+    fh = io.BytesIO()
+    media = MediaIoBaseDownload(fh, descargar)
+    done = False
+    while done is False:
+        status, done = media.next_chunk()
+        print( "Download %d%%." % int(status.progress() * 100))
+    
+    ruta = input("Ingrese la ruta de la ubicacion donde quiere descargar el archivo ")
+
+    fh.seek(0)
+
+    with open(os.path.join(ruta, nombre_archivo), "wb") as archivo:
+        archivo.write(fh.read())
+        archivo.close()
+
+subir_archivo("TP2_1C2021.pdf")
